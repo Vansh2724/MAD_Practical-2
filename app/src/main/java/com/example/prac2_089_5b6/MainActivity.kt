aadb7caf2270a89037ea6f12ba336ebab1ef2fb6
Admin <Admin@123gmail.com>
2023-08-03 12:35:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prac2_089_5b6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prac2_089_5b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
