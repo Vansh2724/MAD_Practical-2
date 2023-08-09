2b003d1ce5071d08102f724abacc7dce28330a2c
Vansh Patel <vanshpatel2724@gmail.com>
2023-08-09 22:16:10 +0530

    9/8/23

--- app/src/main/java/com/example/prac2_089_5b6/MainActivity.kt
+++ app/src/main/java/com/example/prac2_089_5b6/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.prac2_089_5b6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,30 +12,27 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onStart() {
         super.onStart()
-        displayMsg("onStart Method Called")
+        displayMsg("Onstart Method called")
     }
-
     override fun onResume() {
         super.onResume()
-        displayMsg("onResume Method Called")
+        displayMsg("OnResume Method called")
     }
-
     override fun onPause() {
         super.onPause()
-        displayMsg("onPause Method Called")
+        displayMsg("onPause Method called")
     }
-
     override fun onStop() {
         super.onStop()
-        displayMsg("onStop Method Called")
+        displayMsg("onStop Method called")
     }
-
     override fun onDestroy() {
         super.onDestroy()
-        displayMsg("onDestroy Method Called")
+        displayMsg("onDestroy Method called")
     }
-
     fun displayMsg(msg:String){
-        Toast.makeText(this, "$msg", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.vkp), msg,Snackbar.LENGTH_SHORT).show()
+        Log.i("MainActivity", msg)
     }
 }
